ec161f7e7e4ea8b7808453efade755b7b05f8c13
Erik Olofsson <erik@olofsson.info>
2023-04-22 14:13:36 +0200

    [Malterlib/Time] Add literal helpers for converting time units to seconds

--- Source/Malterlib_Time_Time.h
+++ Source/Malterlib_Time_Time.h
@@ -1845,3 +1845,73 @@ namespace NMib::NStream
 	};
 
 }
+
+constexpr inline pfp64 operator ""_µs(long double _Value)
+{
+	return _Value * 0.000'001;
+}
+
+constexpr inline pfp64 operator ""_ms(long double _Value)
+{
+	return _Value * 0.001;
+}
+
+constexpr inline pfp64 operator ""_seconds(long double _Value)
+{
+	return _Value;
+}
+
+constexpr inline pfp64 operator ""_minutes(long double _Value)
+{
+	return _Value * 60.0;
+}
+
+constexpr inline pfp64 operator ""_hours(long double _Value)
+{
+	return _Value * 60.0 * 60.0;
+}
+
+constexpr inline pfp64 operator ""_days(long double _Value)
+{
+	return _Value * 60.0 * 60.0 * 24.0;
+}
+
+constexpr inline pfp64 operator ""_weeks(long double _Value)
+{
+	return _Value * 60.0 * 60.0 * 24.0 * 7.0;
+}
+
+constexpr inline pfp64 operator ""_µs(unsigned long long _Value)
+{
+	return pfp64(_Value) * 0.000'001;
+}
+
+constexpr inline pfp64 operator ""_ms(unsigned long long _Value)
+{
+	return pfp64(_Value) * 0.001;
+}
+
+constexpr inline pfp64 operator ""_seconds(unsigned long long _Value)
+{
+	return pfp64(_Value);
+}
+
+constexpr inline pfp64 operator ""_minutes(unsigned long long _Value)
+{
+	return pfp64(_Value) * 60.0;
+}
+
+constexpr inline pfp64 operator ""_hours(unsigned long long _Value)
+{
+	return pfp64(_Value) * 60.0 * 60.0;
+}
+
+constexpr inline pfp64 operator ""_days(unsigned long long _Value)
+{
+	return pfp64(_Value) * 60.0 * 60.0 * 24.0;
+}
+
+constexpr inline pfp64 operator ""_weeks(unsigned long long _Value)
+{
+	return pfp64(_Value) * 60.0 * 60.0 * 24.0 * 7.0;
+}
